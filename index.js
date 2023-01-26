abb6bc6b5bbe10f34b32902be3a231499ad76a56
Jurayev2509 <jurayevazizbek2525@gmail.com>
2023-01-26 18:41:00 +0500

    Home work lesson 1 in java script

---
+++ index.js
@@ -0,0 +1,43 @@
+////////////Home work//////////////
+
+///////////1 Java scriptda 8 data type lar mavjud ////////
+
+///////////Ular 2 hil turga bolinadi ////////
+
+//////////Primitive /////////
+
+//////////Nun primitive
+
+/////////Primitive ga kiradigon data typelar /
+
+//Number,- bunga raqamlar va NaN  kiradi , Nan bu raqamligini anglatib turadi
+//bigint,- bu true ,yoki false qiymat qaytaradi TRUE bu bor degani , falase bu yoq degan manoni anglatadi
+//string,  "" '' `` shu belgilar srtinga kiradi
+//bolean,  katta raqamlar kiradi 323232323432321323434331312 n   + 1  n qilib yoziladi bunga yana raqam qoshmohchi bolsak
+//Null, yoqlik qiymatga ega hech nima yoq degani
+//Undefiend, buham yoqlik degan qiymatga oxhshash narsa
+//Sumbols
+
+/////////NuN primitive//////////////
+
+//////////Object kiradi va uni ichidagilar   ///////
+///////// Object, Array , Function , kiradi
+
+/////////////Vareibllar //////////////
+
+/////////// Var, const, let, ////////
+
+////////// "Var" bu global hamma ishlatsa boladi boshqa honalardaham  //////////
+
+////////// "let" bu ozgaruvchi hisoblanadi bitta honada ishlatsa boladi  kopicha hozir hamma LET ishlatishmoqda /////
+
+///////// "const"  bu ozgartirib bolmaydi bunga nitta veriable bergandan keyn ozgaruvchi qabul qilmaydi
+
+let name = "Ilya";
+console.log(name); // consolda Ilya chiqadi 
+
+console.log(`Hello ${1}`);  //consolda  Hello1
+
+console.log(`hello ${"name"}` ); // consolda "hello name "chiqadi chunki 
+
+console.log(`hello ${name}`); // consolda hello ILya chiqadi 
